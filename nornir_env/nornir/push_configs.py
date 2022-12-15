c80a1143556c50011c54454db57203a7bc2f164b
housepbass9664 <80693460+housepbass9664@users.noreply.github.com>
2022-12-15 17:14:06 -0500

    Update push_configs.py
    Changed dry_run to False

--- nornir_env/nornir/push_configs.py
+++ nornir_env/nornir/push_configs.py
@@ -9,7 +9,7 @@ nr = InitNornir(config_file="config.yaml")
 def generate_config_and_push(task):
     """Render unique device configuration and push to device"""
     rendered_config = task.run(task=template_file, template=f"{task.host.platform}", path="templates/").result
-    configure_devices = task.run(task=napalm_configure, dry_run=True, configuration=rendered_config)
+    configure_devices = task.run(task=napalm_configure, dry_run=False, configuration=rendered_config)
 
 results = nr.run(task=generate_config_and_push)
 print_result(results)
